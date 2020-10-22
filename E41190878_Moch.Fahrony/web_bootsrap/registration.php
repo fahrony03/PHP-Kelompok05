f86fb48240b26873fa6cbc1815d4f8585141388e
fahrony03 <72066155+fahrony03@users.noreply.github.com>
2020-10-22 15:00:35 +0700

    up

--- E41190878_Moch.Fahrony/web_bootsrap/registration.php
+++ E41190878_Moch.Fahrony/web_bootsrap/registration.php
@@ -221,7 +221,7 @@
         
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 class="h3 mb-0 text-gray-800">Contact Us</h1>
+          <h1 class="h3 mb-0 text-gray-800">Registration</h1>
         </div>
 
         <!-- Content Row -->
