b54c9f39fadc1481b21d8cf4de0d214561fbe785
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2020-10-27 19:25:02 +0700

    Nurico Vicyyanto

---
+++ E41190321_Nurico Vicyyanto/belajar_php/constanta.php
@@ -0,0 +1,12 @@
+<?
+define('VERSION', '1.0.0');
+
+//membuat konstanta
+const SITE_NAME = "petanikode";
+const BASE_URL = "https://www.petanikode.com";
+
+//cetak nilai konstanta
+echo "Site name:".SITE_NAME."<br/>";
+echo "URL:".BASE_URL."<br/>";
+echo "Version:".VERSION."<br/>";
+?>
---
+++ E41190321_Nurico Vicyyanto/belajar_php/print.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<h2>Belajar PHP dari Nol</h2>";
+    echo("Hello world <br>");
+    echo "Belajar mencetak teks di PHP!";
+?>
