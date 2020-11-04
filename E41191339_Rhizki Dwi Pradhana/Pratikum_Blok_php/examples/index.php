3eb352fe9ba3494fd4a21c595065445d3af7beaf
RhizkiPradhana <71806229+RhizkiPradhana@users.noreply.github.com>
2020-11-04 17:32:50 +0700

    pratikum

--- E41191339_Rhizki Dwi Pradhana/Pratikum_Blok_php/examples/index.php
+++ E41191339_Rhizki Dwi Pradhana/Pratikum_Blok_php/examples/index.php
@@ -38,29 +38,45 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user">
-                    <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                      </div>
-                    </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
-                    <hr>
-                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
+                  <?php
+                  session_start();
+                  $username = 'admin';
+                  $password = 'admin';
+                  if (isset($_POST['submit'])) {
+                      if ($_POST['username'] == $username && $_POST['password'] == $password){
+                          //Membuat Session
+                          $_SESSION["username"] = $username; 
+                          echo "Anda Berhasil Login $username";
+                          // Jika Ingin Pindah Ke Halaman Lain
+                          header("Location: dashboard.php");  //Pindahkan Kehalaman Admin
+                      } else {
+                          // Tampilkan Pesan Error
+                          display_login_form();
+                          echo '<p>Username Atau Password Tidak Benar</p>';
+                      }
+                  }    
+                  else { 
+                      display_login_form();
+                  }
+                  function display_login_form(){ ?>
+                      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'>
+                      <div class="form-group">
+                      <input type="nama" class="form-control form-control-user" name ="username" id="username" placeholder="Enter Email Address...">
+                   </div>
+                          <div class="form-group">
+                            <input type="password" class="form-control form-control-user" name="password" id="exampleInputPassword" placeholder="Password">
+                          </div>
+                      <input type="submit" name="submit" value="submit">
+                      </form>    
+                  <?php } ?>
+
+                  <hr>
+                  <a href="index.html" class="btn btn-google btn-user btn-block">
+                    <i class="fab fa-google fa-fw"></i> Login with Google
+                  </a>
+                  <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                    <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                  </a>
                   </form>
                   <hr>
                   <div class="text-center">
@@ -68,7 +84,7 @@
                   </div>
                   <div class="text-center">
                     <a class="small" href="register.html">Create an Account!</a>
-                    
+
                   </div>
                 </div>
               </div>
