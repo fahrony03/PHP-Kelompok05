437e6d3128abf6a26fd692edc721df8f842cf526
fahrony03 <72066155+fahrony03@users.noreply.github.com>
2020-11-04 17:30:34 +0700

    nyar

--- E41190878_Moch.Fahrony/Praktikum Blok Day 3/Tugas Diskon/examples/index.php
+++ E41190878_Moch.Fahrony/Praktikum Blok Day 3/Tugas Diskon/examples/index.php
@@ -34,29 +34,45 @@
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
+              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user">
-                    <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                      </div>
-                    </div>
-                    <a href="dasboard.php" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
-                    <hr>
+                    <?php
+                  session_start();
+                  $username = 'admin';
+                  $password = 'admin';
+                  if (isset($_POST['submit'])) {
+                      if ($_POST['username'] == $username && $_POST['password'] == $password){
+                          //Membuat Session
+                          $_SESSION["username"] = $username; 
+                          echo "Anda Berhasil Login $username";
+                          // Jika Ingin Pindah Ke Halaman Lain
+                          header("Location: dasboard.php");  //Pindahkan Kehalaman Admin
+                      } else {
+                          // Tampilkan Pesan Error
+                          display_login_form();
+                          echo '<p>Username Atau Password Tidak Benar</p>';
+                      }
+                  }    
+                  else { 
+                      display_login_form();
+                  }
+                  function display_login_form(){ ?>
+                      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'>
+                      <div class="form-group">
+                      <input type="nama" class="form-control form-control-user" name ="username" id="username" placeholder="Enter Email Address...">
+                   </div>
+                          <div class="form-group">
+                            <input type="password" class="form-control form-control-user" name="password" id="exampleInputPassword" placeholder="Password">
+                          </div>
+                          <a>
+                      <input type="submit" name="submit" value="submit">
+                  </a>
+                      </form>    
+                  <?php } ?>
                     <a href="#" class="btn btn-google btn-user btn-block">
                       <i class="fab fa-google fa-fw"></i> Login with Google
                     </a>
