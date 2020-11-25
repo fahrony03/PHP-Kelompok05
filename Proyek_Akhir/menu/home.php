da22f3f243e84c884f63475bf2145d8e222f74dc
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2020-11-25 08:29:21 +0700

    update

--- Proyek_Akhir/menu/home.php
+++ Proyek_Akhir/menu/home.php
@@ -1,14 +1,1 @@
 <?php include('nav.php'); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="..\style.css">
-</head>
-<body>
-    <p>ini home</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem natus repellendus suscipit quo nemo velit facilis dolor iusto unde error esse cupiditate necessitatibus autem, impedit amet placeat tenetur laboriosam porro.</p>
-</body>
-</html>
