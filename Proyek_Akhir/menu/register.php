2be9149b5e4b0fe78ab32ca1263f7238e57005ae
RhizkiPradhana <71806229+RhizkiPradhana@users.noreply.github.com>
2020-12-02 11:33:00 +0700

    regis

--- Proyek_Akhir/menu/register.php
+++ Proyek_Akhir/menu/register.php
@@ -41,7 +41,7 @@ if ($datakode) {
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
     integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-  <link rel="stylesheet"  href="../register.css">
+  <link rel="stylesheet"  href="../css/register.css">
 </head>
 <body>
   <div class="container">
