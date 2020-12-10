974f67f4a5c72cf8a031821351e1de64f4a51c38
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2020-12-10 13:43:37 +0700

    update

--- Proyek_Akhir/menu/navbar.php
+++ Proyek_Akhir/menu/navbar.php
@@ -85,9 +85,9 @@
                                 Activity Log
                             </a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="logout.php" data-toggle="modal" data-target="#logoutModal">
-                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Logout
+                            <a class="dropdown-item" href="logout.php">
+                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Log Out
                             </a>
                         </div>
                     </li>
