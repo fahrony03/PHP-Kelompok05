b2c780f668be7c35ad09cace52e166fcd3625998
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2020-12-10 13:54:57 +0700

    Update detailinfo_satu.php

--- Proyek_Akhir/menu/detailinfo_satu.php
+++ Proyek_Akhir/menu/detailinfo_satu.php
@@ -33,7 +33,7 @@
     <?php
     include_once('navbar.php')
 ?>
-<br>
+    <br>
     <div class="container">
         <div class="card">
             <div class="card-body">
@@ -59,39 +59,59 @@
                     <h1>Cara Menanam Wortel Di Rumah Yang Baik Dan Benar. Sangat Mudah! <b></b></h1>
                     <br>
                     <p>
-                    Tanaman Wortel (Daucus carota L.) berasal dari Asia Timur dan Tengah yang memiliki iklim sub-tropis. Tanaman Wortel 
-                    berbentuk semak (perdu) yang tumbuh tegak dengan ketinggian antara 30 cm-100 cm.
-                    Tanaman Wortel memiliki umbi berwarna jingga kemerahan berbentuk lonjong yang sering dimakan sebagai sumber gizi terutama 
-                    vitamin A. Warna jingga kemerahan pada umbi Wortel ini menandakan bahwa itu kaya akan senyawa karoten dan flavonoid yang 
-                    dapat berfungsi sebagai antioksidan.Umbi Wortel sering dikonsumsi sebagai tambahan pada masakan seperti sup ataupun bihun. 
-                    Umbi ini disenangi banyak orang di kalangan masyarakat karena rasanya yang enak dan gizinya yang berlimpah.
-                    Kali ini Si PEKA akan memberikan uraian lengkap mengenai cara menanam wortel yang baik dan benar.
-                    Yuk, simak penjelasannya di bawah ini!.
+                        Tanaman Wortel (Daucus carota L.) berasal dari Asia Timur dan Tengah yang memiliki iklim
+                        sub-tropis. Tanaman Wortel
+                        berbentuk semak (perdu) yang tumbuh tegak dengan ketinggian antara 30 cm-100 cm.
+                        Tanaman Wortel memiliki umbi berwarna jingga kemerahan berbentuk lonjong yang sering dimakan
+                        sebagai sumber gizi terutama
+                        vitamin A. Warna jingga kemerahan pada umbi Wortel ini menandakan bahwa itu kaya akan senyawa
+                        karoten dan flavonoid yang
+                        dapat berfungsi sebagai antioksidan.Umbi Wortel sering dikonsumsi sebagai tambahan pada masakan
+                        seperti sup ataupun bihun.
+                        Umbi ini disenangi banyak orang di kalangan masyarakat karena rasanya yang enak dan gizinya yang
+                        berlimpah.
+                        Kali ini Si PEKA akan memberikan uraian lengkap mengenai cara menanam wortel yang baik dan
+                        benar.
+                        Yuk, simak penjelasannya di bawah ini!.
                     </p>
                     <p>
                         <img src="../img/penanaman/wortel 2.jpg" class="img-fluid" alt="Responsive image">
                     </p>
                     <h3> Jenis Wortel</h3>
-                    <p>Jenis Imperator yaitu jenis wortel yang memiliki bentuk bulat memanjang dengan ujung meruncing seperti kerucut. Wortel ini 
-                    sedikit tipis, serta memiliki umbi berwarna oranye. Wortel jenis ini kurang disukai oleh orang karena rasa yang dimiliki 
-                    wortel jenis ini kurang manis.</p>
-                    <p>Jenis Chantenay. Jenis wortel ini memiliki bentuk tebal meruncing dengan ujung yang tumpul. Jenis wortel Chantenay memiliki 
-                    panjang sekitar 12 cm serta banyak disukai orang karena memiliki rasa yang manis. Dibandingkan jenis wortel lainnya wortel 
-                    jenis ini memiliki tekstur lebih kasar. Biasanya, wortel jenis ini sering dimasak dengan di potong berbentuk dadu.</p> 
-                    <p>Jenis Mantes. Wortel jenis ini merupakan gabungan dari wortel jenis Imperator dan jenis Chantenay. Wortel jenis ini memiliki 
-                    ukuran sedang, memiliki warna oranye gelap, berbentuk bulat tumpul dan memiliki diameter dari atas hingga bawah sama. Kandungan 
-                    air pada wortel nantes lebih tinggi dari jenis wortel lainnya sehingga jika dimasak rasa manis pada wortel jenis ini akan berkurang</p> 
+                    <p>Jenis Imperator yaitu jenis wortel yang memiliki bentuk bulat memanjang dengan ujung meruncing
+                        seperti kerucut. Wortel ini
+                        sedikit tipis, serta memiliki umbi berwarna oranye. Wortel jenis ini kurang disukai oleh orang
+                        karena rasa yang dimiliki
+                        wortel jenis ini kurang manis.</p>
+                    <p>Jenis Chantenay. Jenis wortel ini memiliki bentuk tebal meruncing dengan ujung yang tumpul. Jenis
+                        wortel Chantenay memiliki
+                        panjang sekitar 12 cm serta banyak disukai orang karena memiliki rasa yang manis. Dibandingkan
+                        jenis wortel lainnya wortel
+                        jenis ini memiliki tekstur lebih kasar. Biasanya, wortel jenis ini sering dimasak dengan di
+                        potong berbentuk dadu.</p>
+                    <p>Jenis Mantes. Wortel jenis ini merupakan gabungan dari wortel jenis Imperator dan jenis
+                        Chantenay. Wortel jenis ini memiliki
+                        ukuran sedang, memiliki warna oranye gelap, berbentuk bulat tumpul dan memiliki diameter dari
+                        atas hingga bawah sama. Kandungan
+                        air pada wortel nantes lebih tinggi dari jenis wortel lainnya sehingga jika dimasak rasa manis
+                        pada wortel jenis ini akan berkurang</p>
 
                     <h3> Budidaya Wortel </h3>
-                    Lahan untuk budidaya wortel harus dibajak atau dicangkul sedalam kurang lebih 40 cm. Kedalaman ini sangat penting mengingat tanaman wortel 
-                    akan dipanen umbinya. Tanah yang gembur memberikan keleluasaan pada umbi untuk tumbuh dengan sempurna. Budidaya wortel pada struktur tanah yang 
-                    keras akan menghambat pertumbuhan umbi. Bentuk umbi menjadi pendek-pendek dan tumbuh cabang pada badan umbi. Setelah tanah digemburkan buat 
-                    bedengan dengan lebar satu meter dan panjang disesuaikan dengan bentuk lahan. Ketinggian bedengan sekitar 20-30 cm. Saat membentuk bedengan, 
-                    campurkan pupuk kompos atau pupuk kandang sebagai pupuk dasar. Dosis pemberian pupuk sebanyak 15-20 ton per hektar. Jumlah tepatnya sesuaikan 
-                    dengan tingkat kesuburan tanah. Buat larikan pada permukaan bedengan untuk menaburkan benih. Jarak antar larikan 20 cm dengan kedalaman sekitar 5 cm.
+                    Lahan untuk budidaya wortel harus dibajak atau dicangkul sedalam kurang lebih 40 cm. Kedalaman ini
+                    sangat penting mengingat tanaman wortel
+                    akan dipanen umbinya. Tanah yang gembur memberikan keleluasaan pada umbi untuk tumbuh dengan
+                    sempurna. Budidaya wortel pada struktur tanah yang
+                    keras akan menghambat pertumbuhan umbi. Bentuk umbi menjadi pendek-pendek dan tumbuh cabang pada
+                    badan umbi. Setelah tanah digemburkan buat
+                    bedengan dengan lebar satu meter dan panjang disesuaikan dengan bentuk lahan. Ketinggian bedengan
+                    sekitar 20-30 cm. Saat membentuk bedengan,
+                    campurkan pupuk kompos atau pupuk kandang sebagai pupuk dasar. Dosis pemberian pupuk sebanyak 15-20
+                    ton per hektar. Jumlah tepatnya sesuaikan
+                    dengan tingkat kesuburan tanah. Buat larikan pada permukaan bedengan untuk menaburkan benih. Jarak
+                    antar larikan 20 cm dengan kedalaman sekitar 5 cm.
 
-                    <footer class="blockquote-footer">SiPEKA <cite title="Source Title"> Cara Menanam Wortel Di Rumah 
-                    Yang Baik Dan Benar. Sangat Mudah!</cite>
+                    <footer class="blockquote-footer">SiPEKA <cite title="Source Title"> Cara Menanam Wortel Di Rumah
+                            Yang Baik Dan Benar. Sangat Mudah!</cite>
                     </footer>
                 </blockquote>
             </div>
@@ -104,42 +124,42 @@
     <div class="container">
         <div class="card">
             <div class="card-body">
-            <div class="form">
+                <div class="form">
                     <form action="" method="post">
-                    <label for="komentar">Komentar</label>
-                    <br>
-                    <textarea class="form-control" id="komentar" name="komentar" rows="3"></textarea>
-            </div>
-            <br>
-        <br>
-        <button type="submit" class="btn btn-success " name="submit">kirim</button>
-        </div>
-                    <?php
-                    error_reporting(E_ALL ^ E_NOTICE);
-                        include "koneksi.php";
-                        $komentar  = $_GET['komentar'];
-                        $mysqli  = "INSERT INTO `komentar` (`komentar`) VALUES ('$komentar')";
-                        $result  = mysqli_query($koneksi, $mysqli);
-                    ?>
-                    </form>
+                        <label for="komentar">Komentar</label>
+                        <br>
+                        <textarea class="form-control" id="komentar" name="komentar" rows="3"></textarea>
                 </div>
+                <br>
+                <br>
+                <button type="submit" class="btn btn-success " name="submit">kirim</button>
             </div>
-        </div>
-        </div>
-            </div>
-        </div>
-    </div>
-    <br>
 
-    <?php
-        include_once('footer.php');
-        ?>
+            <?php
+//Include file koneksi ke database
+                include "koneksi.php";
+                $komentar=$_POST["komentar"];
+                $sql="insert into komentar (komentar) values('$komentar')";
+                $hasil=mysqli_query($koneksi,$sql);
+                if ($hasil) {
+                    echo "Berhasil insert data";
+                    exit;
+                }
+                else {
+                    echo "Gagal insert data";
+                    exit;
+                }
+                ?>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <a href="#"
-        class="float" target="_blank">
-        <i class="fa fa-whatsapp my-float"></i>
-    </a>
+
+            <?php
+                include_once('footer.php');
+                ?>
+
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+            <a href="#" class="float" target="_blank">
+                <i class="fa fa-whatsapp my-float"></i>
+            </a>
 
 </body>
 
