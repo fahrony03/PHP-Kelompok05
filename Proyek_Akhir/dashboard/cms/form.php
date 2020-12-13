3d89267bed9f8c218e442a527ff4e35e372366dd
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2020-12-14 06:41:17 +0700

    update

--- Proyek_Akhir/dashboard/cms/form.php
+++ Proyek_Akhir/dashboard/cms/form.php
@@ -3,9 +3,12 @@
   <title>Form Upload Gambar</title>
 </head>
 <body>
+
   <h1>Form Upload Gambar</h1>
   <form method="post" enctype="multipart/form-data" action="upload.php">
     <input type="file" name="gambar">
+    <input type="text" name="title">
+    <input type="text" name="content">
     <input type="submit" value="Upload">
   </form>
 </body>
