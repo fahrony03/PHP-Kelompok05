985c0d592ae2896c9bb72e915d40f530398da223
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2020-12-14 08:46:50 +0700

    Update navbar.php

--- Proyek_Akhir/menu/navbar.php
+++ Proyek_Akhir/menu/navbar.php
@@ -70,32 +70,7 @@
                             <li><a class="dropdown-item" href="logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
                         </ul>
 
-                    <div class="topbar-divider d-none d-sm-block"></div>
-                    <li class="nav-item dropdown no-arrow">
-                        <a class="nav-link" href="#" id="userDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img class="img-profile rounded-circle img-responsive" src="../img/ser.png">
-                            <span class="mr-2 d-none d-lg-inline text-white-600 ">
-                            <?php echo $_SESSION['username']; ?>
-                            </span>
-                        </a>
-                        <!-- Dropdown - User Information -->
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                            aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="auth/profil.php">
-                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Profile
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="auth/logout.php">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Log Out
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-                </ul>
-            </div>
+                    
     </nav>
     <br><br><br>
 
