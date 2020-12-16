113c5ab30bebf6dfcbaa777148f2b4914082c732
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2020-12-17 05:13:09 +0700

    fix

--- Proyek_Akhir/admin/menu/informasi/index.php
+++ Proyek_Akhir/admin/menu/informasi/index.php
@@ -29,16 +29,7 @@
       ?>
     <main class="app-content">
       <div class="app-title">
-        <div>
-          <h1><i class="fa fa-edit"></i> Form Components</h1>
-          <p>Bootstrap default form components</p>
-        </div>
-        <ul class="app-breadcrumb breadcrumb">
-          <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-          <li class="breadcrumb-item">Forms</li>
-          <li class="breadcrumb-item"><a href="#">Form Components</a></li>
-        </ul>
-      </div>
+    
       <div class="row">
         <div class="col-md-12">
           <div class="tile">
