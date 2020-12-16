ca705773a0ef2c5950de77d1a73da680d3a313a7
Moch. Fahrony <72066155+fahrony03@users.noreply.github.com>
2020-12-16 14:02:28 +0700

    up

--- Proyek_Akhir/admin/index.php
+++ Proyek_Akhir/admin/index.php
@@ -85,7 +85,7 @@
           <ul class="dropdown-menu settings-menu dropdown-menu-right">
             <li><a class="dropdown-item" href="page-user.php"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
             <li><a class="dropdown-item" href="page-user.php"><i class="fa fa-user fa-lg"></i> Profile</a></li>
-            <li><a class="dropdown-item" href="../menu/login.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
+            <li><a class="dropdown-item" href="../menu/auth/login.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
           </ul>
         </li>
       </ul>
