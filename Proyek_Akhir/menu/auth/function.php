03a00c1b0abaefc2683693d91414d71c15ab20a6
RhizkiPradhana <71806229+RhizkiPradhana@users.noreply.github.com>
2020-12-23 09:57:49 +0700

    function gbr

--- Proyek_Akhir/menu/auth/function.php
+++ Proyek_Akhir/menu/auth/function.php
@@ -51,15 +51,42 @@ function registrasi($data)
     }
     // enkripsi password
     // $password = password_hash($password, PASSWORD_DEFAULT);
+    
+    }
+    // menyimpan gambar
+    $path = "images/".$nama_file;
+    if (!empty($_FILES["file_gbr"]["tmp_name"]))
+    {
+    $jenis_gambar=$_FILES['file_gbr']['type']; //memeriksa format gambar
+    if($jenis_gambar=="image/jpeg" || $jenis_gambar=="image/jpg" || $jenis_gambar=="image/gif" ||
+    $jenis_gambar=="image/png")
+    {
+    $lampiran = $namafolder . basename($_FILES['file_gbr']['name']);
+
+    //mengupload gambar dan update row table database dengan path folder dan nama image
+    if (move_uploaded_file($_FILES['file_gbr']['tmp_name'], $lampiran)) {
+    }
     // tambahkan user baru ke database
-    mysqli_query($conn, "INSERT INTO user VALUES ('$kode_user', '$nama_user', '$username', '$password', '$email', '$no_hp','$level')");
+    mysqli_query($conn, "INSERT INTO user VALUES ('$kode_user', '$nama_user', '$username', '$password', '$email', '$no_hp', '$lampiran','$level')");
     echo "
             <script>
                     alert('anda berhasil daftar!');
                     
             </script>
 
-            ";
+            <br /><br />
+            <img src='$lampiran' height='300'>";
 
-    return mysqli_affected_rows($conn);
+            //Jika gagal upload, tampilkan pesan Gagal
+            } else {
+            echo "Gambar gagal dikirim";
+            }
+            } else {
+            echo "Jenis gambar yang anda kirim salah. Harus .jpg .gif .png";
+            }
+            } else {
+            echo "Anda belum memilih gambar";
+            }
+
+                return mysqli_affected_rows($conn);
 }
