eb96fb552d50a01ffa24d0f50f6111ada418ac16
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2021-01-06 18:42:34 +0700

    update

--- Proyek_Akhir/admin/menu/Belanja/hapus.php
+++ Proyek_Akhir/admin/menu/Belanja/hapus.php
@@ -1,7 +1,7 @@
 <?php
         require "koneksi.php";
                  if (isset($_GET['delete'])) {
-                $query = $pdo->prepare ("DELETE FROM penanaman WHERE id = {$_GET['delete']}");
+                $query = $pdo->prepare ("DELETE FROM belanja WHERE id = {$_GET['delete']}");
                  
                     if ($query->execute()){
                     header('Location:index.php');
--- Proyek_Akhir/admin/menu/informasi/hapus.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'koneksi.php';
-$id = $_GET['id'];
-mysqli_query($connect,"delete from informasi where id='$id'");
-
-header("location:index.php?pesan=hapus");
-?>
